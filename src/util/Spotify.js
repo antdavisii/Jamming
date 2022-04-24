2814e25c48f53691fc70173c32d4bc8a0a456ac8
Antoine Davis <antoinedavistwo@outlook.com>
2022-04-23 20:08:05 -0500

    Obtain a Spotify Access Token setup

---
+++ src/util/Spotify.js
@@ -0,0 +1,30 @@
+const clientId = '50177a1f3d90471b92bf0cbef0408342';
+const redirectUri = 'http://localhost:3000/';
+
+let accessToken;
+
+const Spotify = {
+	getAccessToken() {
+		if (accessToken) {
+			return accessToken;
+		}
+
+		// check for an access token match
+		const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+		const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+		if (accessTokenMatch && expiresInMatch) {
+			accessToken = accessTokenMatch[1];
+			const expiresIn = Number(expiresInMatch[1]);
+			// This clears the parameters, allowing us to grab a new access token when it expires.
+			window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
+			window.history.pushState('Access Token', null, '/');
+			return accessToken;
+		} else {
+			const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+			window.location = accessUrl;
+		}
+	},
+};
+
+export default Spotify;
